a5a7a4aac5f2768bd4f925b6c298193c1144513d
onmedicines <semwalanurag26@gmail.com>
2024-09-18 15:20:18 +0530

    Add loader function to load books in db

--- src/pages/AllBooks.jsx
+++ src/pages/AllBooks.jsx
@@ -1,3 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+import Book from "../components/Book";
+
 export default function AllBooks() {
-  return <div>AllBooks</div>;
+  const books = useLoaderData();
+
+  return (
+    <div className="all-books-container">
+      {books.map((book) => (
+        <Book key={book._id} bookName={book.name} author={book.author} imgurl={"bookcover.jpg"} />
+      ))}
+    </div>
+  );
 }
+
+// loader
+export const booksLoader = async () => {
+  const apiResponse = await fetch("http://localhost:3000/all-books");
+  return await apiResponse.json();
+};
