15ec402d9ab7fcaeda78f45c23559b5b7c1ee289
onmedicines <semwalanurag26@gmail.com>
2024-09-18 15:41:22 +0530

    Add Form.jsx to add book to to db

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import RootLayout from "./layouts/RootLayout";
+import AllBooks, { booksLoader } from "./pages/AllBooks";
 
 export default function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
         <Route index element={<Home />} />
+        <Route path="all-books" element={<AllBooks />} loader={booksLoader} />
       </Route>
     )
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,21 @@
 export default function Form() {
-  return <div>Form</div>;
+  return (
+    <div className="form-container">
+      <form action="/" method="post">
+        <label htmlFor="bookName">
+          <span>Book:</span>
+          <input type="text" name="bookName" id="bookName" placeholder="eg. Harry Potter" />
+        </label>
+        <label htmlFor="authorName">
+          <span>Author:</span>
+          <input type="text" name="authorName" id="authorName" placeholder="eg. JK Rowling" />
+        </label>
+        <label htmlFor="pages">
+          <span>Pages:</span>
+          <input type="number" name="pages" id="pages" />
+        </label>
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  );
 }
