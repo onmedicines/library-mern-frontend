eecdae311f3be62bf9ee2df5e642698765982819
onmedicines <semwalanurag26@gmail.com>
2024-09-18 15:19:10 +0530

    Add Book.jsx to display each book in AllBooks.jsx

---
+++ src/components/Book.jsx
@@ -0,0 +1,9 @@
+export default function Book({ bookName, author, imgurl }) {
+  return (
+    <div className="book-container">
+      <img src={imgurl} alt="book" />
+      <h1>{bookName}</h1>
+      <p>{author}</p>
+    </div>
+  );
+}
